78a1e898de0bad0278bdebea8bfe99dbb61aa580
sevasu2 <babagame7@gmail.com>
2019-02-05 12:42:39 +0000

    いいね機能完了

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,5 +2,12 @@ class PostImage < ApplicationRecord
 	belongs_to :user
 	attachment :image # ここを追加（_idは含めません）
 	has_many :post_comments, dependent: :destroy
+	has_many :favorites, dependent: :destroy
+
+	 def favorited_by?(user)
+          favorites.where(user_id: user.id).exists?
+          # このメソッドで、引数で渡されたユーザのidがFavoritesテーブル内に存在（exists?）しているかを調べます。
+          # 存在していればTrue、存在していなければFalseを返すようにしています。
+     end
 
 end
