3e9753395080261111634be2c0ecb98a63ca1f6d
sevasu2 <babagame7@gmail.com>
2019-02-05 12:11:51 +0000

    コメント機能完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+	devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'post_images#index'
-  resources :post_images, only:[:new, :index, :show, :create]
+  resources :post_images, only:[:new, :index, :show, :create]do
+ 	 resource :post_comments, only: [:create, :destroy]
+ 	 # 単数にするとそのコントローラのidがリクエストに含まれなくなります。
+ 	 # post_commentsのshowページは必要ない（コメントの詳細ページは作成しない）ため、
+ 	 # idを受け渡す必要がなくresourceとしています。
+  end
 
 
 end
