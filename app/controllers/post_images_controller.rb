dc69076614cf67b6c074d11030777a39b33ed9bc
sevasu2 <babagame7@gmail.com>
2019-02-05 13:15:56 +0000

    post_imageにバリデーションを追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,8 +8,17 @@ class PostImagesController < ApplicationController
 	def create
 		@post_image = PostImage.new(post_image_params)
 		@post_image.user_id = current_user.id
-		@post_image.save
-		redirect_to post_images_path
+		if @post_image.save
+		  redirect_to post_images_path
+		  # redirect_to：HTTPリクエストを送るため、ルーティングから処理が行われます。
+		else
+          render :new
+          render：他のビューを呼び出す。
+			# ※通常、呼び出されるビューは呼び出し元のコントローラ名とアクション名で決まるが、
+			# renderを使うことで他のビューが呼び出せます。
+			# この際、呼び出し先でインスタンス変数が使われていると、
+			# 呼び出し元で定義されていなければエラーになるので注意が必要です。
+        end
 	end
 
 	def index
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,11 @@ class PostImage < ApplicationRecord
 	has_many :post_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
 
+	validates :shop_name, presence: true
+    validates :image, presence: true
+    # バリデーションによって、対象のカラム（ここではimageとshop_name）に
+    # データが入力されていなければ、saveメソッドでfalseが返ります
+
 	 def favorited_by?(user)
           favorites.where(user_id: user.id).exists?
           # このメソッドで、引数で渡されたユーザのidがFavoritesテーブル内に存在（exists?）しているかを調べます。
