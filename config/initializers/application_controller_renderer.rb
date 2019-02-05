675cf6b832a5888a6ef561dedfc3a71bda9483cb
sevasu2 <babagame7@gmail.com>
2019-02-05 14:14:10 +0000

    バグ修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,5 +6,3 @@
 #     https: false
 #   )
 # end
-
-Refile.secret_key = '7d61c20a93c7d69317bcbe6f0fd14c588fb2e46e54c505db03b0e1f60baf36b71d9ffe2f1c7d2492daef7dbc3324e47d616d69af41281670b75fafea215e53ef'
